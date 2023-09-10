2d3788698b9f9508660f25c7849cebf7302e4111
dreslafat <dreslafat@gmail.com>
2023-09-10 13:33:33 -0300

    Mi Primer Commit

---
+++ js/popup.js
@@ -0,0 +1,111 @@
+var abrirVamp = document.getElementById('abrir-vamp'),
+    overlayVamp = document.getElementById('overlay-vamp'),
+    cerrarVamp = document.getElementById('cerrar-vamp');
+
+abrirVamp.addEventListener('click', function()
+{
+    overlayVamp.classList.add('active')
+})
+
+cerrarVamp.addEventListener('click', function()
+{
+    overlayVamp.classList.remove('active')
+})
+
+var abrirMini = document.getElementById('abrir-mini'),
+    overlayMini = document.getElementById('overlay-mini'),
+    cerrarMini = document.getElementById('cerrar-mini');
+
+abrirMini.addEventListener('click', function()
+{
+    overlayMini.classList.add('active')
+})
+
+cerrarMini.addEventListener('click', function()
+{
+    overlayMini.classList.remove('active')
+})
+
+var abrirfxv3 = document.getElementById('abrir-fxv3'),
+    overlayfxv3 = document.getElementById('overlay-fxv3'),
+    cerrarfxv3 = document.getElementById('cerrar-fxv3');
+
+abrirfxv3.addEventListener('click', function()
+{
+    overlayfxv3.classList.add('active')
+})
+
+cerrarfxv3.addEventListener('click', function()
+{
+    overlayfxv3.classList.remove('active')
+})
+
+var abrirgoat = document.getElementById('abrir-goat'),
+    overlaygoat = document.getElementById('overlay-goat'),
+    cerrargoat = document.getElementById('cerrar-goat');
+
+abrirgoat.addEventListener('click', function()
+{
+    overlaygoat.classList.add('active')
+})
+
+cerrargoat.addEventListener('click', function()
+{
+    overlaygoat.classList.remove('active')
+})
+
+var abrirwah = document.getElementById('abrir-wah'),
+    overlaywah = document.getElementById('overlay-wah'),
+    cerrarwah = document.getElementById('cerrar-wah');
+
+abrirwah.addEventListener('click', function()
+{
+    overlaywah.classList.add('active')
+})
+
+cerrarwah.addEventListener('click', function()
+{
+    overlaywah.classList.remove('active')
+})
+
+var abrirlofi6 = document.getElementById('abrir-lofi6'),
+    overlaylofi6 = document.getElementById('overlay-lofi6'),
+    cerrarlofi6 = document.getElementById('cerrar-lofi6');
+
+abrirlofi6.addEventListener('click', function()
+{
+    overlaylofi6.classList.add('active')
+})
+
+cerrarlofi6.addEventListener('click', function()
+{
+    overlaylofi6.classList.remove('active')
+})
+
+var abrirlofi4 = document.getElementById('abrir-lofi4'),
+    overlaylofi4 = document.getElementById('overlay-lofi4'),
+    cerrarlofi4 = document.getElementById('cerrar-lofi4');
+
+abrirlofi4.addEventListener('click', function()
+{
+    overlaylofi4.classList.add('active')
+})
+
+cerrarlofi4.addEventListener('click', function()
+{
+    overlaylofi4.classList.remove('active')
+})
+
+var abrirkids3 = document.getElementById('abrir-kids3'),
+    overlaykids3 = document.getElementById('overlay-kids3'),
+    cerrarkids3 = document.getElementById('cerrar-kids3');
+
+abrirkids3.addEventListener('click', function()
+{
+    overlaykids3.classList.add('active')
+})
+
+cerrarkids3.addEventListener('click', function()
+{
+    overlaykids3.classList.remove('active')
+})
